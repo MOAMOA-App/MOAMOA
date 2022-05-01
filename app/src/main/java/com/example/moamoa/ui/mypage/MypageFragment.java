76ec71f4e6178dd2536a5b28de4f95ec7705ccd8
lhs0127 <hi9374328@gmail.com>
2022-05-01 18:01:29 +0900

    마이페이지 수정

--- app/src/main/java/com/example/moamoa/ui/mypage/MypageFragment.java
+++ app/src/main/java/com/example/moamoa/ui/mypage/MypageFragment.java
@@ -3,6 +3,7 @@ package com.example.moamoa.ui.mypage;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,20 +26,24 @@ import com.example.moamoa.databinding.FragmentMyPageBinding;
 
 public class MypageFragment extends Fragment {
 
-    private FragmentMyPageBinding binding;
-    static final String[] LIST_MENU = {"내 정보 수정", "만든 폼 관리", "참여한 폼 관리", "찜 목록", "앱 정보", "환경설정"} ;
+    static final String[] LIST_MENU1 = {"✏️  만든 폼 관리", "✔   참여한 폼 관리", "\uD83E\uDDE1   관심 목록"} ;
+    static final String[] LIST_MENU2 = {"    내 정보 수정", "⚙️  환경설정"} ;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_my_page, null) ;
 
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU) ;
+        ArrayAdapter adapter1 = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU1) ;
+        ArrayAdapter adapter2 = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU2) ;
 
-        ListView listview = (ListView) view.findViewById(R.id.listView) ;
-        listview.setAdapter(adapter) ;
+        ListView listview1 = (ListView) view.findViewById(R.id.listView1) ;
+        listview1.setAdapter(adapter1) ;
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ListView listview2 = (ListView) view.findViewById(R.id.listView2) ;
+        listview2.setAdapter(adapter2) ;
+
+        listview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(view.getContext(), "메세지", Toast.LENGTH_SHORT).show();
@@ -52,23 +58,24 @@ public class MypageFragment extends Fragment {
                     transaction.addToBackStack(null);
                     transaction.commit();
                 }
-                if(position == 2){
+                if(position == 2) {
                     FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                     CreatedForms fragment2 = new CreatedForms();
                     transaction.replace(R.id.my_page, fragment2);
                     transaction.addToBackStack(null).commit();
                 }
-                if(position == 3){
-                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                    CreatedForms fragment2 = new CreatedForms();
-                    transaction.replace(R.id.my_page, fragment2);
-                    transaction.addToBackStack(null).commit();
-                }
-                if(position == 4){
+            }
+        });
+
+        listview2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(view.getContext(), "메세지", Toast.LENGTH_SHORT).show();
+                if(position == 0){
                     Intent intent = new Intent(getActivity(), EditMyinfo.class);
                     startActivity(intent);
                 }
-                if(position == 5){
+                if(position == 1){
                     FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                     Setting fragment3 = new Setting();
                     transaction.replace(R.id.my_page, fragment3);
@@ -80,10 +87,15 @@ public class MypageFragment extends Fragment {
         return view ;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
+
+//리스트뷰 글씨색 바꾸기
+// @Override
+//            public View getView(int position, View convertView, ViewGroup parent)
+//            {
+//                View view = super.getView(position, convertView, parent);
+//                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+//                tv.setTextColor(Color.rgb(250,143,57));
+//                return view;
+//            }
 
