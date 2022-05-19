7665ea8702497d238c46ceb5d41e5167206b4058
LGRIN-byte <66896175+LGRIN-byte@users.noreply.github.com>
2022-05-19 15:17:03 +0900

    카테고리 화면 이동 및 탭

---
+++ category/ListFragment.java
@@ -0,0 +1,24 @@
+package com.example.moamoa.ui.category;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.moamoa.R;
+
+
+
+public class ListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_listup, container, false);
+        return rootView;
+    }
+}
+
