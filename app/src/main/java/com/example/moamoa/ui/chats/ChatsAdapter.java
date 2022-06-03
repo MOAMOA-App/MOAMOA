2a862630912b5593fc11f1750c1ce729c7e3aa4d
tasty13 <bodri0625@gmail.com>
2022-06-03 15:05:56 +0900

    채팅창+채팅리스트 3차수정 채팅 DB 연결/사용언어정보 메뉴에서 확인할 수 있게 따로 뺌((KOR) 이런거/채팅 보낸시간 표시(ing)/채팅방향 다르게 수정 메뉴에서 참여자 확인가능하게(1~2차) 글씨크기 전반적으로 줄임(1차)

--- app/src/main/java/com/example/moamoa/ui/chats/ChatsAdapter.java
+++ app/src/main/java/com/example/moamoa/ui/chats/ChatsAdapter.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moamoa.R;
@@ -16,15 +18,18 @@ import java.util.List;
 public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.Holder> {
     private Context context;
     private List<ChatsData> list = new ArrayList<>();
+    private String leftname;
 
-    public ChatsAdapter(Context context, List<ChatsData> list){
+    public ChatsAdapter(Context context, List<ChatsData> list, String leftname){
         this.context = context;
         this.list = list;
+        this.leftname = leftname;
     }
 
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_chats, parent, false);
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_chats, parent, false);
         Holder holder = new Holder(view);
         return holder;
     }
@@ -32,31 +37,66 @@ public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.Holder> {
     @Override
     public void onBindViewHolder(Holder holder, int position) {
         // 각 위치에 문자열 세팅
-        int itemposition = position;
-        holder.leftNickName.setText(list.get(itemposition).leftname);
-        holder.leftMessage.setText(list.get(itemposition).leftmessage);
-        holder.rightNickName.setText(list.get(itemposition).rightname);
-        holder.rightMessage.setText(list.get(itemposition).rightmessage);
+
+        ChatsData chats = list.get(position);
+        holder.nickName.setText(chats.nickname);
+        holder.Message.setText(chats.chatmsg);
+        holder.sendedTime.setText(chats.sendedtime);
+
+        if (list.get(position).getLeftname().equals(this.leftname)){
+            // 내가 보낸 메시지일시 오른쪽에서 출력:왼쪽 이미지
+            holder.nickName.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.Message.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.sendedTime.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            holder.profile_image.setVisibility(View.INVISIBLE); //프사 안보이게
+        }
+        else{
+            // 남이 보낸 메시지일시 왼쪽에서 출력
+            holder.nickName.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.Message.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            holder.sendedTime.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+        }
+
     }
 
+    // Return the size of your dataset (invoked by the layout manager)
     // 몇개의 데이터를 리스트로 뿌려줘야하는지 반드시 정의해줘야한다
     @Override
     public int getItemCount() {
-        return list.size();
-    }   // RecyclerView의 size return
+        //return list.size();  //RecyclerView의 size return
+        //삼항 연산자
+        return list == null ? 0 :  list.size();
+    }
+
+    public ChatsData getChat(int position) {
+        return list != null ? list.get(position) : null;
+    }
+
+    public void addChat(ChatsData chat) {
+        list.add(chat);
+        notifyItemInserted(list.size()-1); //갱신
+        // EX: 0, 1, 2 = 3번지. ~>오류날수도.
+        // 사이즈가 3개면 3번지에 넣으란 뜻인데 저기에는 3번지 없음--> 2에 넣는다(list.size()-1)
+    }
 
     public class Holder extends RecyclerView.ViewHolder{
-        public TextView leftNickName;
-        public TextView leftMessage;
-        public TextView rightNickName;
-        public TextView rightMessage;
+        public TextView nickName, Message, sendedTime;
+        ImageView profile_image;
+        public View rootView;
+        //CardView cv;
 
         public Holder(View view){
             super(view);
-            leftNickName = (TextView) view.findViewById(R.id.TextView_nickname_left);
-            leftMessage = (TextView) view.findViewById(R.id.TextView_message_left);
-            rightNickName = (TextView) view.findViewById(R.id.TextView_nickname_right);
-            rightMessage = (TextView) view.findViewById(R.id.TextView_message_right);
+            nickName = (TextView) view.findViewById(R.id.chat_nickname);
+            Message = (TextView) view.findViewById(R.id.chat_msg);
+            sendedTime = (TextView) view.findViewById(R.id.sended_time);
+            //cv = (CardView) view.findViewById(R.id.chat_cardview);
+
+            profile_image = (ImageView) view.findViewById(R.id.profile_image);
+
+            rootView = view;
+
+
         }
     }
 }
