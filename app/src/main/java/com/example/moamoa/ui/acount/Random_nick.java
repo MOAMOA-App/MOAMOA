5a5ce2be1fcdc25083c553cf7f9cea20b1f67457
nasong <tmddms0317@naver.com>
2022-06-13 22:37:58 +0900

    detail 디자인 변경 random_nick 수정

--- app/src/main/java/com/example/moamoa/ui/acount/Random_nick.java
+++ app/src/main/java/com/example/moamoa/ui/acount/Random_nick.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Random_nick {
-    ArrayList<String> nickarray = new ArrayList<String>(Arrays.asList("무지개","분홍","오렌지","개나리","연두","해변의","퍼렁","하얀"));
-    ArrayList<String> chararray = new ArrayList<String>(Arrays.asList("곰돌이","귀긴곰","꽥","웨옹","고양이","곰","토끼","오리","고양이"));
+    ArrayList<String> nickarray = new ArrayList<String>(Arrays.asList("무지개","분홍","오렌지","개나리","연두","해변의","퍼렁","보라","갈색","하얀"));
+    ArrayList<String> chararray = new ArrayList<String>(Arrays.asList("웨옹","곰돌이","귀긴곰","꽥","고양이","고양이","곰","토끼","오리"));
     private String nickname;
     private String image;
     public void Random_nick(){
--- app/src/main/java/com/example/moamoa/ui/formdetail/FormdetailActivity.java
+++ app/src/main/java/com/example/moamoa/ui/formdetail/FormdetailActivity.java
@@ -2,17 +2,22 @@ package com.example.moamoa.ui.formdetail;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.moamoa.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class FormdetailActivity extends Activity {
     private DatabaseReference mDatabase;
@@ -49,33 +54,29 @@ public class FormdetailActivity extends Activity {
         return;
     }
     private void UserFind(String UID){
+
+
         mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
         mDatabase.child(UID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                TextView name_text = (TextView) findViewById(R.id.detail_nick);
-                ImageView profil_text = (ImageView) findViewById(R.id.detail_profile);
-                //name_text.setText(dataSnapshot.child("image").getValue().toString());
-                name_text.setText(dataSnapshot.child("name").getValue().toString());
-                /*
-                DatabaseReference mDatabase;
-                mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
-                mDatabase.child(item.getNick()).child("nick").addValueEventListener(new ValueEventListener() {
+                String name;
+                String profil_text;
+                TextView name_tv = (TextView) findViewById(R.id.detail_nick);
+                ImageView profile = (ImageView) findViewById(R.id.detail_profile);
+                profil_text = dataSnapshot.child("image").getValue().toString();
+                name = dataSnapshot.child("nick").getValue().toString();
+                name_tv.setText(name);
+                FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+                StorageReference pathReference = firebaseStorage.getReference(profil_text);
+                pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        viewHolder.txt_name.setText((dataSnapshot.getValue().toString()));
-
+                    public void onSuccess(Uri uri) {
+                        Glide.with(profile)
+                                .load(uri)
+                                .into(profile);
                     }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        // Getting Post failed, log a message
-                        Log.w("", "loadPost:onCancelled", databaseError.toException());
-                        // ...
-                    }
-
                 });
-
-                 */
             }
 
             @Override
@@ -85,6 +86,7 @@ public class FormdetailActivity extends Activity {
                 // ...
             }
         });
+
     }
     private void Initializeform(String subject,String category,String text,String cost,String max_count) {
         TextView subject_text = (TextView) findViewById(R.id.detail_subject);
