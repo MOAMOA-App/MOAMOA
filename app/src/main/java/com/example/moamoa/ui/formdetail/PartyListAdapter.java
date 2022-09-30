3f51689ca8ef367726bf7fc896636af09e20026e
nasong <kimse990507@gmail.com>
2022-09-30 17:06:42 +0900

    참여자리스트 완성 공지하기 -> 공지 생성 완성        -> 공지 출력 진행중

--- app/src/main/java/com/example/moamoa/ui/formdetail/PartyListAdapter.java
+++ app/src/main/java/com/example/moamoa/ui/formdetail/PartyListAdapter.java
@@ -1,20 +1,27 @@
 package com.example.moamoa.ui.formdetail;
 
+import android.app.Activity;
 import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.moamoa.R;
 import com.example.moamoa.ui.home.homelist_adapter;
 import com.example.moamoa.ui.home.homelist_data;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -44,8 +51,9 @@ public class PartyListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int pos, View view, ViewGroup viewGroup) {
-        CircleImageView profile_img = (CircleImageView) view.findViewById(R.id.party_profileimage);
+    public View getView(int pos, View converview, ViewGroup viewGroup) {
+        View view = mLayoutInflater.inflate(R.layout.partylist_parts, null);
+        ImageView profile_img = (ImageView) view.findViewById(R.id.party_profileimage);
         TextView nickname   = (TextView) view.findViewById(R.id.party_nickname);
         TextView lastchat   = (TextView) view.findViewById(R.id.party_lastchat);
         TextView partdate   = (TextView) view.findViewById(R.id.party_date);
@@ -53,46 +61,19 @@ public class PartyListAdapter extends BaseAdapter {
         nickname.setText(list.get(pos).getNickname());
         lastchat.setText(list.get(pos).getLastchat());
         partdate.setText(list.get(pos).getParti_date());
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference pathReference = firebaseStorage.getReference(list.get(pos).getProfile());
+        Activity context2 = (Activity) profile_img.getContext();
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                if (context2.isFinishing()) return;
+                Glide.with(profile_img.getContext())
+                        .load(uri)
+                        .into(profile_img);
+            }
+        });
         return view;
     }
-    /*
-    private ArrayList<PartyListData> list = new ArrayList<>();
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        private homelist_adapter.OnItemClickListener mListener = null;
-        public CircleImageView profile_img;
-        public TextView nickname, lastchat, parti_date;
-        public ViewHolder(View view) {
-            super(view);
-            profile_img = (CircleImageView) view.findViewById(R.id.party_profileimage);
-            nickname = (TextView) view.findViewById(R.id.party_nickname);
-            parti_date = (TextView) view.findViewById(R.id.party_date);
-            lastchat = (TextView) view.findViewById(R.id.party_lastchat);
-
-
-        }
-    }
-    public PartyListAdapter(ArrayList<PartyListData> arrayList) {
-        this.list = list;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.partylist_parts, parent,false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        PartyListData item = list.get(position);
-        Log.e("adf",item.getNickname());
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-    */
 }
