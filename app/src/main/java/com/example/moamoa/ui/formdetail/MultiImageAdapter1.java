1109eeb12ee032fd9f69bafefbc552c79658a1f9
lgrin-byte <lgrin02@naver.com>
2022-10-28 22:28:44 +0900

    사진 다중 출력 방향 수정 크기 미정

--- app/src/main/java/com/example/moamoa/ui/formdetail/MultiImageAdapter1.java
+++ app/src/main/java/com/example/moamoa/ui/formdetail/MultiImageAdapter1.java
@@ -1,5 +1,6 @@
 package com.example.moamoa.ui.formdetail;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -11,18 +12,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.moamoa.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 /*
-* 이미지 여러개 넣기 위한 어뎁터
-* */
-public class MultiImageAdapter extends RecyclerView.Adapter<MultiImageAdapter.ViewHolder>{
-    private ArrayList<Uri> mData = null ;
+ * 이미지 여러개 넣기 위한 어뎁터
+ * */
+public class MultiImageAdapter1 extends RecyclerView.Adapter<MultiImageAdapter1.ViewHolder>{
+    private ArrayList<String> mData = null ;
     private Context mContext = null ;
 
     // 생성자에서 데이터 리스트 객체, Context를 전달받음.
-    public MultiImageAdapter(ArrayList<Uri> list, Context context) {
+    public MultiImageAdapter1(ArrayList<String> list, Context context) {
         mData = list ;
         mContext = context;
     }
@@ -56,12 +60,18 @@ public class MultiImageAdapter extends RecyclerView.Adapter<MultiImageAdapter.Vi
     // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Uri image_uri = mData.get(position) ;
-
-        Glide.with(mContext)
-                .load(image_uri)
-                .into(holder.image);
-    }
+        String image_uri = mData.get(position);
+        StorageReference pathReference = FirebaseStorage.getInstance().getReference(image_uri);
+        Activity context = (Activity) holder.image.getContext();
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                if (context.isFinishing()) return;
+                Glide.with(mContext)
+                        .load(uri)
+                        .into(holder.image);
+            }
+        });}
 
     // getItemCount() - 전체 데이터 갯수 리턴.
     @Override
